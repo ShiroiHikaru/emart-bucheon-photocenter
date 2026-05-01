625ba7a44569af4fd32bf831099ac0c990fa7527
heeyeon <asd95121@gmail.com>
2026-05-01 23:38:04 +0900

    feat: pretendard 폰트 추가, link css 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/Footer/Footer";
 
+
 export default function Home() {
   return (
     <main>
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -5,15 +5,15 @@ export default function Header(){
   return(
     <header className={S.header}>
       <div className={S.logo} aria-label="로고" role="메인페이지">
-        <Link href="/" className="return-home">
-          <span className="sr-only">메인으로 이동</span>
+        <Link href="/" className={S.return_home}>
+          <span className="sr-only" aria-label="메인이동" role="index이동">메인으로 이동</span>
         </Link>
       </div>
       <nav className={S.nav}>
-        <a href="#pricing">가격 안내</a>
-        <a href="#faq">자주 묻는 질문</a>
-        <button>🌙</button>
-        <button>☀️</button>
+        <Link href="#pricing" className={S.linkPage}>가격 안내</Link>
+        <Link href="#faq" className={S.linkPage}>자주 묻는 질문</Link>
+        <button type="button" aria-label="theme-다크모드" role="다크모드">🌙</button>
+        <button type="button" aria-label="theme-라이트모드" role="라이트모드">☀️</button>
       </nav>
     </header>
   )
